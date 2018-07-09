1b6e0d6d99139a47c1fcc9cdfc11a676396879e3
Faiz A Farooqui <faizahmed@Faizs-MacBook-Air.local>
2018-07-09 18:28:25 +0530

    added: cli library with man/help/stats commands

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
@@ -20,6 +21,10 @@ app.init = () => {
   // Start the workers
   workers.init();
 
+  // Start the CLI, but make sure it starts last
+  setTimeout(() => {
+  	cli.init();
+  }, 500);
 };
 
 // Self executing
