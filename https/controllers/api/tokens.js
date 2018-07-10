53bda8fe7eb63496882a028c4442a3b89262d6d9
Faiz A Farooqui <faizahmed@Faizs-MacBook-Air.local>
2018-07-10 12:24:11 +0530

    added: performance & performance observer classes from perf_hooks modules

--- https/controllers/api/tokens.js
+++ https/controllers/api/tokens.js
@@ -9,22 +9,50 @@ const _data = require('./../../../lib/data');
 const helpers = require('./../../../lib/helpers');
 const config = require('./../../../lib/config');
 
+const _performance = require('perf_hooks').performance;
+const _performanceObserver = require('perf_hooks').PerformanceObserver;
+const util = require('util');
+const debug = util.debuglog('performance');
+
+// Start observing the performance from start
+const obs = new _performanceObserver((list, observer) => {
+  // Get the list of all performance measures
+  const entries = list.getEntries();
+  entries.forEach((entry) => {
+    // Log out all the measurements
+    debug('\x1b[33m%s\x1b[0m', entry.name + ': ' + entry.duration);
+  });
+
+  // Disconnect observers
+  obs.disconnect();
+});
+
+// Fetch all the "measure" module performance
+obs.observe({entryTypes: ['measure'], buffered: true});
+
 // Tokens handler methods
 var _tokens = {
   // POST
   // Required data: phone, password
   // Optional data: none
   post: (data, callback) => {
+    _performance.mark('entered function');
     var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
     var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    _performance.mark('inputs validated');
     if (phone && password) {
+      _performance.mark('beginning user lookup');
       // Lookup the user who matches that phone number
       _data.read('users', phone, (err,userData) => {
+        _performance.mark('user lookup complete');
         if (!err && userData) {
           // Hash the sent password, and compare it to the password stored in the user object
+          _performance.mark('beginning password hashing');
           var hashedPassword = helpers.hash(password);
+          _performance.mark('password hashing complete');
           if (hashedPassword == userData.hashedPassword) {
             // If valid, create a new token with a random name. Set an expiration date 1 hour in the future.
+            _performance.mark('creating data for the token');
             var tokenId = helpers.createRandomString(20);
             var expires = Date.now() + 1000 * 60 * 60;
             var tokenObject = {
@@ -34,7 +62,18 @@ var _tokens = {
             };
 
             // Store the token
+            _performance.mark('beginning storing token');
             _data.create('tokens', tokenId, tokenObject, (err) => {
+              _performance.mark('storing token complete');
+
+              // Gather all the measurements
+              _performance.measure('Validating User Input', 'entered function', 'inputs validated');
+              _performance.measure('User Lookup', 'beginning user lookup', 'user lookup complete');
+              _performance.measure('Password Hashing', 'beginning password hashing', 'password hashing complete');
+              _performance.measure('Token Data Creation', 'creating data for the token', 'beginning storing token');
+              _performance.measure('Token Storing', 'beginning storing token', 'storing token complete');
+              _performance.measure('Beginning to End', 'entered function', 'storing token complete');
+
               if (! err) {
                 callback(200,tokenObject);
               } else {
